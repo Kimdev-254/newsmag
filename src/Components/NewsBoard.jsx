8efe538ba14ab3941c4fd573f4012764dde4a3fe
Kimdev254 <your-email@example.com>
2024-09-11 23:58:44 +0300

    trending

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -17,16 +17,14 @@ const NewsBoard = ({ category }) => {
   return (
     <div>
       <h2 className="text-center ">
-        Latest
-        <span className="badge bg-danger text-dark fs-3">
-          Upcoming  News
-        </span>
+        Trending Latest
+        <span className="badge bg-danger text-dark fs-3">Upcoming News</span>
       </h2>
       {articles.map((news, index) => {
         return (
           <NewsItem
             key={index}
-              title={news.title}
+            title={news.title}
             description={news.description}
             src={news.urlToImage}
             url={news.url}
